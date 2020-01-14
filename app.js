4ca81064a9ee37bfc1832c54a9900ce07bcc32aa
J. Garijo <jogarijo@gmail.com>
2020-01-14 10:19:06 +0100

    Define workflow locales master (private) and fr-fr (default)

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ const path = require('path');
 module.exports = require('apostrophe')({
   shortName: 'repl-apostrophe-boilerplate',
   modules: {
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
+    'apostrophe-workflow': {
+      defaultLocale: 'fr-fr',
+      locales: [
+        {
+          name: 'master',
+          label: 'Master',
+          private: true,
+          children: [
+            {
+              name: 'fr-fr',
+              label: 'France',
+            },
+          ],
+        },
+      ],
+      prefixes: {
+        master: '/master',
+      },
+      hostnames: {
+        master: 'lvh.me',
+        'fr-fr': `fr-fr.lvh.me`,
+      },
+    }
   }
 });
